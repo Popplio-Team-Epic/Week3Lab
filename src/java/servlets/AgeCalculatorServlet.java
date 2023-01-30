8356f56a1972fdc14bf4f12d572d56e70d7bb56f
Andrea <Andrea@Ezekiels-Laptop-Mark-Two.cg.shawcable.net>
2023-01-30 02:26:16 -0700

    Version 1.0 Copied code structure from Week3Demo and adjusted to fit the lab contents. agecalculator.jsp works as intended, but AgeCalculatorServlet is not working.

---
+++ src/java/servlets/AgeCalculatorServlet.java
@@ -0,0 +1,52 @@
+
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AgeCalculatorServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String age = request.getParameter("age");
+        String responseMessage = request.getParameter("responseMessage");
+        int ageValid;
+        
+        // Validation Check - Null
+        if (age == null || age.equals("")) {
+            responseMessage = "You must give your current age.";
+            request.setAttribute("responseMessage", responseMessage);
+            return;
+        }
+        // Validation Check - String
+        // https://www.studytonight.com/java-examples/check-if-input-is-integer-in-java
+        try {
+            Integer.parseInt(age);
+            // Valid
+            ageValid = Integer.valueOf(age);
+        }
+        // Invalid
+        catch (NumberFormatException e) {
+            responseMessage = "You must enter a number.";
+            request.setAttribute("responseMessage", responseMessage);
+            return;
+        }
+        
+        // Proccessed Correctly
+        ageValid = ageValid + 1;
+        responseMessage = "Your age next birthday will be " + ageValid;
+        request.setAttribute("responseMessage", responseMessage);
+    }
+
+}
---
+++ web/WEB-INF/agecalculator.jsp
@@ -0,0 +1,22 @@
+<%-- 
+    Document   : agecalculator
+    Created on : 30-Jan-2023, 1:49:23 AM
+    Author     : Andrea
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Age Calculator</title>
+    </head>
+    <body>
+        <h1>Age Calculator</h1>
+        <form action="hello" method="post">
+            Enter your age: <input type="text" name="age" value="${age}"><br>
+            <input type="submit" value="Age next birthday">
+        </form><br>
+        <p>${responseMessage}</p>
+    </body>
+</html>
